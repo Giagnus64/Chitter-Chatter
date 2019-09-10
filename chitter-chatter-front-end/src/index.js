0fb1625450e599b127233f732761ffdc14e67ea8
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-09-10 16:37:08 -0400

    Added message posting feature

--- chitter-chatter-front-end/src/index.js
+++ chitter-chatter-front-end/src/index.js
@@ -2,59 +2,170 @@
 //Login/Edit/Register Vars
 const loginFormDiv = document.querySelector(".user-login-form-div-css");
 const editFormDiv = document.querySelector(".user-edit-form-div-css");
-const usernameDisplay = document.querySelector(".username-message-form");
-const currentUserImage = document.querySelector("#current-user-image");
 const loginFormName = document.querySelector("#login-username-input")
 const loginForm = document.querySelector(".login-form-js");
-
-// Event Listeners
+//User-Container-Vars
+const userContainer = document.querySelector("#user-container-js")
+//Register
 const registerForm = document.querySelector(".register-form")
 const registerFormName = document.querySelector("#register-username-input")
 const registerFormImage = document.querySelector("#register-icon-input")
 
+//chatbox vars
+const chattingWith = document.querySelector("#chatting-with-js")
+const chatBoxMessages = document.querySelector("#chat-messages-js")
 
-//User-Container-Vars
-const userContainer = document.querySelector("#user-container-js")
+//message form vars
+const usernameDisplay = document.querySelector(".username-message-form");
+const currentUserImage = document.querySelector("#current-user-image");
+const messageForm = document.querySelector("#message-form-js");
+const messageInput = document.querySelector("#message-input-js")
 
 
+//Other Vars
+const usersUrl = "http://localhost:3000/users";
+const messagesUrl = "http://localhost:3000/messages"
 
-
+//Event Listeners
 loginForm.addEventListener('submit', handleLogin)
 registerForm.addEventListener('submit', handleRegister)
+userContainer.addEventListener("click", createChat)
+messageForm.addEventListener('submit', handleMessage)
 
 // Functions
+//message form functions
+function handleMessage(event){
+    event.preventDefault();
+    // check if the user is chatting with someone
+    if(chattingWith.textContent !== "" && messageInput.value !== ""){
+       sendMessage();
+    }
+    else{
+        alert("Please select a user first!")
+    }
+
+}
+function sendMessage(){
+    fetch(messagesUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            content: messageInput.value,
+            sender_id: chattingWith.dataset.userId,
+            reciever_id: usernameDisplay.dataset.currentUserId
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        messageForm.reset();
+        addSentMessageToChat(data)});
+}
+
+//chat functions
+function createChat(event){
+    if (event.target.classList.contains("user-card-js")){
+        const chattingUser = event.target.dataset.userId
+        addIconToChat(event);
+        getMessages(chattingUser);
+    }
+}
+
+function addIconToChat(event){
+    if (event.target.classList.contains("user-card-div")){
+        let copy = event.target.outerHTML;
+        chattingWith.innerHTML = copy;
+        chattingWith.dataset.userId = event.target.dataset.userId
+    }
+    else {
+        let copy = event.target.parentElement.outerHTML;
+        chattingWith.innerHTML = copy;
+        chattingWith.dataset.userId = event.target.dataset.userId
+    }
+}
+
+function getMessages(chattingUserId){
+    const currentUserId = usernameDisplay.dataset.currentUserId
+    console.log(chattingUserId)
+    fetch(usersUrl + `/${currentUserId}`)
+    .then(res => res.json())
+    .then(data => {
+        const sorted = handleMessages(chattingUserId, data.sent_messages, data.recieved_messages)
+        addAllMessagesToChat(sorted, currentUserId);
+    })
+}
+
+function filterSentMessages(chattingUserId, msgArray){
+    return msgArray.filter(function(message){
+        return message.reciever_id === parseInt(chattingUserId)
+    })
+}
+
+function filterRecievedMessages(chattingUserId, msgArray){
+    return msgArray.filter(function(message){
+        return message.sender_id === parseInt(chattingUserId)
+    })
+}
+
+function sortMessages(msgArray){
+    return msgArray.sort((a, b) => a.created_at - b.created_at);
+}
+
+function handleMessages(chattingUserId, sentMessages, recievedMessages){
+    const sent = filterSentMessages(chattingUserId, sentMessages);
+    const recieved = filterRecievedMessages(chattingUserId, recievedMessages);
+    const msgArray = [...sent, ...recieved];
+    const sorted = sortMessages(msgArray);
+    return sorted; 
+}
+
+
+function addAllMessagesToChat(data, currentUserId){
+    chatBoxMessages.innerHTML = "";
+    data.forEach(function(message){
+        if (message.sender_id === currentUserId){
+            addSentMessageToChat(message)
+        }
+        else{
+            addRecievedMessageToChat(message)
+        }
+    });
+
+}
+
+function addSentMessageToChat(message) {
+    chatBoxMessages.innerHTML += `<div class="message-card message-card-sent">
+    <p data-message-id=${message.id} class="message">${message.content}</p>
+    <button data-message-id=${message.id} class="replay-message-button">Replay</button>
+    </div>` 
+}
+
+function addRecievedMessageToChat(message) {
+    chatBoxMessages.innerHTML += `<div class="message-card message-card-recieved">
+    <p data-message-id=${message.id} class="message">${message.content}</p>
+    <button data-message-id=${message.id} class="replay-message-button">Replay</button>
+    </div>` 
+}
+
 // Generate User Icons
 
 function generateUserIcons(data){
     data.forEach(user =>{
-        userContainer.innerHTML += `<div data-user-id=${user.id} class="user-card">
-    <img class="user-card-icon" src="${user.icon_url}">
-    <p class="user-card-username">${user.username}</p>
+        userContainer.innerHTML += `<div data-user-id=${user.id} class="user-card user-card-js user-card-div">
+    <img data-user-id=${user.id} class="user-card-icon user-card-js" src="${user.icon_url}">
+    <p data-user-id=${user.id} class="user-card-username user-card-js">${user.username}</p>
         </div>`
     })
     
 }
 
 function getUsers(){
-    fetch("http://localhost:3000/users")
+    fetch(usersUrl)
     .then(res => res.json())
     .then(data => generateUserIcons(data))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -66,7 +177,7 @@ function handleLogin(event){
         alert("Username cannot be empty")
         return true 
     }
-    fetch("http://localhost:3000/users")
+    fetch(usersUrl)
     .then(res => res.json())
     .then(data => {
         const userObj = data.find(e=>{return e.username === usernameEntered})
@@ -83,19 +194,19 @@ function handleRegister(event){
         alert("Username cannot be empty")
         return true
     }
-    fetch("http://localhost:3000/users")
+    fetch(usersUrl)
     .then(res => res.json())
     .then(data=>{
         const userObj = data.find(e => { return e.username === usernameEntered })
         if (userObj){alert("Username already taken")}
-        else {PostUserToDatabase(usernameEntered, registerFormImage.value)} 
+        else {postUserToDatabase(usernameEntered, registerFormImage.value)} 
     })   
 }
 
 function postUserToDatabase(username, image){
     let defaultUrl = "https://www.writeups.org/wp-content/uploads/Harry-Potter-Philosopher-Stone-era.jpg"
     let imageUrl = (image === "") ? defaultUrl : image
-    fetch("http://localhost:3000/users", {
+    fetch(usersUrl, {
         method: "POST",
         headers: {"Content-Type":"application/json"},
         body: JSON.stringify({username: username, icon_url: imageUrl})
